0ba11206d1ee3d86e333f8fc7ba5b8c07f3c187e
Certer001 <76663981+Certer001@users.noreply.github.com>
2022-07-16 00:50:02 +0300

    blizko k koncy

--- JS/js.js
+++ JS/js.js
@@ -1,6 +1,6 @@
-var location = 3;
-var location = 4;
-var location = 5;
+var location1 = 3;
+var location2 = 4;
+var location3 = 5;
 
 var guess; //undefine
 var hits = 0;
@@ -9,10 +9,19 @@ var guesses = 0;
 var isSunk = false;
 
 while(isSunk == false){
-     guess = prompt('Ns готов выстрелить? (введи цифру 0-6;');
+     guess = prompt('ты готов выстрелить? (введи цифру 0-6)');
      if(guess < 0 || guess > 6){
      	alert('ну еб твою мать я просил от 0 до 6!!!');
      } else{
      	guesses = guesses + 1;
+     	if(guess == location1 || guess == location2 || guess == location3){
+            hits = hits +1;
+            if(hits == 3){
+              isSunk = true;
+              alert('ты победил (хорош)')
+            }
+     	}
      }
 }
+var status = 'вы выстрелили' + guesses + 'для того чтобы попасть по кораблю, ' + 
+'это показывает уровень твоей точности, как: ' + (3/guesses);
